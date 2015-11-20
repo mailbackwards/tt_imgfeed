ff2ea576b75af9654f4f9ebdb531f7c58306c4b0
Liam Andrew <landrew@texastribune.org>
2015-11-19 20:47:41 -0600

    Add feed title

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 DEFAULT_FEED = 'main'
 
 def get_feed(url):
-    return feedparser.parse(url).entries
+    feed = feedparser.parse(url)
+    title = feed.feed.get('title', '')
+    if title and title.startswith('The Texas Tribune'):
+        title = ' '.join(title.split(' ')[3:])
+    return {
+        'title': title,
+        'entries': feed.entries
+    }
 
 def make_soup(item):
     return BeautifulSoup(item.summary, 'html.parser')
@@ -19,9 +26,9 @@ def respond():
 @app.route('/<path:path>')
 def hello_world(path):
     url = 'https://www.texastribune.org/feeds/'+path
-    items = get_feed(url)
+    feed = get_feed(url)
     struct_items = []
-    for item in items:
+    for item in feed['entries']:
         soup = make_soup(item)
         img = soup.find('img') or {}
         if not img:
@@ -31,7 +38,7 @@ def hello_world(path):
             'img': img.get('src'),
             'date': ' '.join(item.published.split(' ')[:4])
         })
-    return render_template('index.html', items=struct_items)
+    return render_template('index.html', items=struct_items, title=feed['title'])
 
 if __name__ == '__main__':
     app.run(debug=True)
